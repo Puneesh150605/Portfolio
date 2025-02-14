604d5159d244141155e93393c105c8da97e34166
Puneesh2005 <puneeshhingorani@gmail.com>
2025-02-15 00:42:05 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,20 @@
 "use client"
 
 import { useEffect } from "react"
-import Header from "@/components/Header"
-import Hero from "@/components/Hero"
-import AboutMe from "@/components/AboutMe"
-import Projects from "@/components/Projects"
-import Skills from "@/components/Skills"
-import Contact from "@/components/Contact"
-import Footer from "@/components/Footer"
+
 import AOS from "aos"
 import "aos/dist/aos.css"
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
+  console.log("fgfgfg");
+  
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -23,12 +26,12 @@ export default function Home() {
   return (
     <main className="bg-gray-900 text-gray-100 min-h-screen">
       <Header />
-      <Hero />
+     <Hero />
       <AboutMe />
       <Projects />
       <Skills />
       <Contact />
-      <Footer />
+      <Footer /> 
     </main>
   )
 }
