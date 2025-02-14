80ff681a3f4907925c70dc4f7bea48c88760a222
Puneesh2005 <puneeshhingorani@gmail.com>
2025-02-15 00:48:30 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true }, // Required for static export
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  images: { unoptimized: true },
   experimental: {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  basePath: "/Portfolio", // Ensure it matches your GitHub repo name
-  assetPrefix: "/Portfolio/",
-}
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+};
 
 export default nextConfig;
