8428d6f1d7b8ccc337306f88ed4682502d213060
Puneesh2005 <puneeshhingorani@gmail.com>
2025-02-16 18:55:51 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
+  output: "export",
   images: { unoptimized: true },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
 export default nextConfig;
